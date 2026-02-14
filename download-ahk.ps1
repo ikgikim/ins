beee34729e6cb5f817faf169bc912555cef517af
Ikgi_Kim <tmasterk@gmail.com>
2026-02-14 19:43:08 +0900

    Update download-ahk.ps1

--- download-ahk.ps1
+++ download-ahk.ps1
@@ -3,6 +3,7 @@ New-Item -ItemType Directory -Path "C:\iks" -Force
 
 # 다운로드
 $response = Invoke-WebRequest -Uri "https://ghins.iks0423.workers.dev/ikv2.ahk"
-$response.Content | Set-Content -Path "C:\iks\ikv2.ahk" -Encoding UTF8
+[byte[]]$utf8Bytes = [System.Text.Encoding]::UTF8.GetBytes($response.Content)
+[System.IO.File]::WriteAllBytes("C:\iks\ikv2.ahk", $utf8Bytes)
 
 Write-Output "ikv2.ahk 다운로드 완료: C:\iks\ikv2.ahk"
